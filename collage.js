2138efce1db2e34c1482f9493c1e7d82766f7492
aprtlt26 <117754660+aprtlt26@users.noreply.github.com>
2024-05-28 11:27:42 -0500

    Update collage.js

--- collage.js
+++ collage.js
@@ -1,234 +1,263 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyB6ATVR7zP3Xuuk62Y6MXa8kCkX7diFVoE",
-    authDomain: "libro-digital-ccf11.firebaseapp.com",
-    projectId: "libro-digital-ccf11",
-    storageBucket: "libro-digital-ccf11.appspot.com",
-    messagingSenderId: "869085974070",
-    appId: "1:869085974070:web:14a6500caf903bd2749297",
-    measurementId: "G-G57LE45QYR"
-  };
-  
-  // Inicializar Firebase
-  firebase.initializeApp(firebaseConfig);
-  const storage = firebase.storage();
 
-  const images = [
-    '0.gif', '01.gif', '02.gif', '03.gif', '04.gif', '05.gif', '06.gif', '07.gif',
-    '08.gif', '09.gif', '10.gif', '11.gif', '02.webp', '03.webp', '04.webp', '05.webp',
-    '06.webp', '07.webp', '08.webp', '09.webp', '10.webp', '11.webp', '12.webp', '14.webp', '15.webp'
-  ];
+        const firebaseConfig = {
+            apiKey: "AIzaSyB6ATVR7zP3Xuuk62Y6MXa8kCkX7diFVoE",
+            authDomain: "libro-digital-ccf11.firebaseapp.com",
+            projectId: "libro-digital-ccf11",
+            storageBucket: "libro-digital-ccf11.appspot.com",
+            messagingSenderId: "869085974070",
+            appId: "1:869085974070:web:14a6500caf903bd2749297",
+            measurementId: "G-G57LE45QYR"
+        };
 
-  const imageAudioMap = {
-    '0.gif': '0.mp3',
-    '01.gif': '01.mp3',
-    '02.gif': '02.mp3',
-    '03.gif': '03.mp3',
-    '04.gif': '04.mp3',
-    '05.gif': '05.mp3',
-    '06.gif': '06.mp3',
-    '07.gif': '07.mp3',
-    '08.gif': '08.mp3',
-    '09.gif': '09.mp3',
-    '10.gif': '10.mp3',
-    '11.gif': '11.mp3',
-    '02.webp': '',
-    '03.webp': '',
-    '04.webp': '',
-    '05.webp': ''
-  };
+       
+        firebase.initializeApp(firebaseConfig);
+        const storage = firebase.storage();
 
-  let collageImageAudioMap = new Map();
-  let audiosImages = [];
+        const images = [
+            '0.gif', '01.gif', '02.gif', '03.gif', '04.gif', '05.gif', '06.gif', '07.gif',
+            '08.gif', '09.gif', '10.gif', '11.gif', '02.webp', '03.webp', '04.webp', '05.webp',
+            '06.webp', '07.webp', '08.webp', '09.webp', '10.webp', '11.webp', '12.webp', '14.webp', '15.webp'
+        ];
 
-  document.addEventListener('DOMContentLoaded', function() {
-    loadImagesFromFirebase();
-  });
+        const imageAudioMap = {
+            '0.gif': '0.mp3',
+            '01.gif': '01.mp3',
+            '02.gif': '02.mp3',
+            '03.gif': '03.mp3',
+            '04.gif': '04.mp3',
+            '05.gif': '05.mp3',
+            '06.gif': '06.mp3',
+            '07.gif': '07.mp3',
+            '08.gif': '08.mp3',
+            '09.gif': '09.mp3',
+            '10.gif': '10.mp3',
+            '11.gif': '11.mp3',
+            '02.webp': '',
+            '03.webp': '',
+            '04.webp': '',
+            '05.webp': ''
+        };
 
-  function loadImagesFromFirebase() {
-    const galleryContainer = document.querySelector('#gallery-container');
-    images.forEach(image => {
-      loadFirebaseImage(image).then(url => {
-        const img = document.createElement('img');
-        img.src = url;
-        img.style.width = '100px';
-        img.addEventListener('click', function(e) {
-          e.stopPropagation();
-          const collageImg = img.cloneNode();
-          collageImg.classList.add('draggable');
-          collageImg.style.position = 'absolute';
-          collageImg.style.left = '0px';
-          collageImg.style.top = '0px';
-          document.getElementById('collage-container').appendChild(collageImg);
-          makeImageInteractive(collageImg);
+        let collageImageAudioMap = new Map();
+        let audiosImages = [];
 
-          const audioSrc = imageAudioMap[image];
-          if (audioSrc) {
-            collageImageAudioMap.set(collageImg, audioSrc);
-            playAudio(audioSrc); // Reproduce el audio cuando se coloca la imagen
-          }
+        document.addEventListener('DOMContentLoaded', function() {
+            loadImagesFromFirebase();
         });
-        galleryContainer.appendChild(img);
-      }).catch(error => {
-        console.error(`Error al cargar la imagen ${image}:`, error);
-      });
-    });
-  }
 
-  function loadFirebaseImage(fileName) {
-    const storageRef = storage.ref();
-    const imageRef = storageRef.child(fileName);
-    return imageRef.getDownloadURL();
-  }
+        function loadImagesFromFirebase() {
+            const galleryContainer = document.querySelector('#gallery-container');
+            images.forEach(image => {
+                loadFirebaseImage(image).then(url => {
+                    const img = document.createElement('img');
+                    img.src = url;
+                    img.style.width = '100px';
+                    img.addEventListener('click', function(e) {
+                        e.stopPropagation();
+                        const collageImg = img.cloneNode();
+                        collageImg.classList.add('draggable');
+                        collageImg.style.position = 'absolute';
+                        collageImg.style.left = '0px';
+                        collageImg.style.top = '0px';
+                        document.getElementById('collage-container').appendChild(collageImg);
+                        makeImageInteractive(collageImg);
 
-  function makeImageInteractive(img) {
-    interact(img)
-      .draggable({
-        listeners: {
-          start(event) {
-            let target = event.target;
-            target.style.zIndex = 1000;
-          },
-          move(event) {
-            let target = event.target,
-                x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
-                y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+                        const audioSrc = imageAudioMap[image];
+                        if (audioSrc) {
+                            collageImageAudioMap.set(collageImg, audioSrc);
+                            playAudio(audioSrc); // Reproduce el audio cuando se coloca la imagen
+                        }
+                    });
+                    galleryContainer.appendChild(img);
+                }).catch(error => {
+                    console.error(`Error al cargar la imagen ${image}:`, error);
+                });
+            });
+        }
 
-            target.style.transform = `translate(${x}px, ${y}px) rotate(${target.getAttribute('data-angle') || 0}deg)`;
+        function loadFirebaseImage(fileName) {
+            const storageRef = storage.ref();
+            const imageRef = storageRef.child(fileName);
+            return imageRef.getDownloadURL();
+        }
 
-            target.setAttribute('data-x', x);
-            target.setAttribute('data-y', y);
-          }
-        },
-        inertia: true,
-        modifiers: [
-          interact.modifiers.restrictRect({
-            restriction: 'parent',
-            endOnly: true
-          })
-        ],
-        autoScroll: true
-      });
+        function makeImageInteractive(img) {
+            interact(img)
+                .draggable({
+                    listeners: {
+                        start(event) {
+                            let target = event.target;
+                            target.style.zIndex = 1000;
+                        },
+                        move(event) {
+                            let target = event.target,
+                                x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+                                y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
 
-    img.addEventListener('dblclick', function() {
-      let angle = parseInt(this.getAttribute('data-angle') || '0');
-      angle = (angle + 180) % 360;
-      this.style.transform = `translate(${this.getAttribute('data-x') || 0}px, ${this.getAttribute('data-y') || 0}px) rotate(${angle}deg)`;
-      this.setAttribute('data-angle', angle.toString());
-    });
-  }
+                            target.style.transform = `translate(${x}px, ${y}px) rotate(${target.getAttribute('data-angle') || 0}deg)`;
 
-  function playAudio(src) {
-    const audio = new Audio(src);
-    audio.play();
-    audiosImages.push(audio);
-  }
+                            target.setAttribute('data-x', x);
+                            target.setAttribute('data-y', y);
+                        }
+                    },
+                    inertia: true,
+                    modifiers: [
+                        interact.modifiers.restrictRect({
+                            restriction: 'parent',
+                            endOnly: true
+                        })
+                    ],
+                    autoScroll: true
+                })
+                .resizable({
+                    edges: { left: true, right: true, bottom: true, top: true },
+                    listeners: {
+                        move(event) {
+                            let target = event.target;
+                            let x = (parseFloat(target.getAttribute('data-x')) || 0);
+                            let y = (parseFloat(target.getAttribute('data-y')) || 0);
 
-  document.getElementById('move-up').addEventListener('click', function() {
-    const selectedImage = document.querySelector('.draggable.selected');
-    if (selectedImage) {
-      let zIndex = parseInt(window.getComputedStyle(selectedImage).zIndex, 10);
-      zIndex = isNaN(zIndex) ? 1 : zIndex + 1;
-      selectedImage.style.zIndex = zIndex;
-    }
-  });
+                            target.style.width = event.rect.width + 'px';
+                            target.style.height = event.rect.height + 'px';
 
-  document.getElementById('move-down').addEventListener('click', function() {
-    const selectedImage = document.querySelector('.draggable.selected');
-    if (selectedImage) {
-      let zIndex = parseInt(window.getComputedStyle(selectedImage).zIndex, 10);
-      zIndex = isNaN(zIndex) ? 0 : zIndex - 1;
-      selectedImage.style.zIndex = zIndex;
-    }
-  });
+                            x += event.deltaRect.left;
+                            y += event.deltaRect.top;
 
-  document.getElementById('print-button').addEventListener('click', function() {
-    window.print();
-  });
+                            target.style.transform = `translate(${x}px, ${y}px) rotate(${target.getAttribute('data-angle') || 0}deg)`;
 
-  document.getElementById('resetButton').addEventListener('click', function() {
-    clearCanvas();
-  });
+                            target.setAttribute('data-x', x);
+                            target.setAttribute('data-y', y);
+                        }
+                    }
+                });
 
-  function clearCanvas() {
-    const collageContainer = document.getElementById('collage-container');
-    const images = collageContainer.querySelectorAll('img');
-    images.forEach(img => {
-      collageContainer.removeChild(img);
-    });
+            img.addEventListener('dblclick', function() {
+                let angle = parseInt(this.getAttribute('data-angle') || '0');
+                angle = (angle + 180) % 360;
+                this.style.transform = `translate(${this.getAttribute('data-x') || 0}px, ${this.getAttribute('data-y') || 0}px) rotate(${angle}deg)`;
+                this.setAttribute('data-angle', angle.toString());
+            });
 
-    audiosImages.forEach(audio => {
-      if (!audio.paused) {
-        audio.pause();
-      }
-    });
-    audiosImages = [];
-    collageImageAudioMap.clear();
-  }
+            img.addEventListener('click', function(e) {
+                if (e.ctrlKey || e.metaKey) {
+                    img.classList.toggle('selected');
+                } else {
+                    document.querySelectorAll('.draggable.selected').forEach(selectedImg => {
+                        if (selectedImg !== img) {
+                            selectedImg.classList.remove('selected');
+                        }
+                    });
+                    img.classList.add('selected');
+                }
+            });
+        }
 
-  document.getElementById('newGameButton').addEventListener('click', function() {
-    window.location.href = 'index.html';
-  });
+        function playAudio(src) {
+            const audio = new Audio(src);
+            audio.play();
+            audiosImages.push(audio);
+        }
 
-  document.getElementById('paintButton').addEventListener('click', function() {
-    window.location.href = 'paint.html';
-  });
+        document.getElementById('move-up').addEventListener('click', function() {
+            const selectedImage = document.querySelector('.draggable.selected');
+            if (selectedImage) {
+                let zIndex = parseInt(window.getComputedStyle(selectedImage).zIndex, 10);
+                zIndex = isNaN(zIndex) ? 1 : zIndex + 1;
+                selectedImage.style.zIndex = zIndex;
+            }
+        });
 
-  const backgroundImages = [
+        document.getElementById('move-down').addEventListener('click', function() {
+            const selectedImage = document.querySelector('.draggable.selected');
+            if (selectedImage) {
+                let zIndex = parseInt(window.getComputedStyle(selectedImage).zIndex, 10);
+                zIndex = isNaN(zIndex) ? 0 : zIndex - 1;
+                selectedImage.style.zIndex = zIndex;
+            }
+        });
 
+        document.getElementById('print-button').addEventListener('click', function() {
+            window.print();
+        });
+
+        document.getElementById('resetButton').addEventListener('click', function() {
+            clearCanvas();
+        });
+
+        function clearCanvas() {
+            const collageContainer = document.getElementById('collage-container');
+            const images = collageContainer.querySelectorAll('img');
+            images.forEach(img => {
+                collageContainer.removeChild(img);
+            });
+
+            audiosImages.forEach(audio => {
+                if (!audio.paused) {
+                    audio.pause();
+                }
+            });
+            audiosImages = [];
+            collageImageAudioMap.clear();
+        }
+
+        document.getElementById('newGameButton').addEventListener('click', function() {
+            window.location.href = 'index.html';
+        });
+
+        document.getElementById('paintButton').addEventListener('click', function() {
+            window.location.href = 'paint.html';
+        });
+
+        const backgroundImages = [
+            'past.webp',
+            'lago.webp',
+            'florez.webp',
+        ];
+
+        let currentIndex = 0;
+
+        function changeBackground() {
+            const collageContainer = document.getElementById('collage-container');
+            collageContainer.style.backgroundImage = `url('${backgroundImages[currentIndex]}')`;
+            collageContainer.style.backgroundSize = 'cover';
+
+            currentIndex = (currentIndex + 1) % backgroundImages.length;
+        }
+
+        document.addEventListener('DOMContentLoaded', changeBackground);
+        document.getElementById('change-background-button').addEventListener('click', changeBackground);
+
+        var storyAudio = new Audio('cuento.mp3');
+        var missionAudio = new Audio('elcollage.wav');
+
+        function playSingleAudio(audio) {
+            storyAudio.pause();
+            storyAudio.currentTime = 0;
+            missionAudio.pause();
+            missionAudio.currentTime = 0;
+
+            audio.play();
+        }
+
+        document.getElementById('storyButton').addEventListener('click', function() {
+            if (storyAudio.paused) {
+                playSingleAudio(storyAudio);
+            }
+        });
+
+        document.getElementById('misionButton').addEventListener('click', function() {
+            if (missionAudio.paused) {
+                playSingleAudio(missionAudio);
+            }
+        });
+
+        document.getElementById('play-button').addEventListener('click', function() {
+            const allImages = document.querySelectorAll('#collage-container img');
+            allImages.forEach(img => {
+                const audioSrc = collageImageAudioMap.get(img);
+                if (audioSrc) {
+                    playAudio(audioSrc);
+                }
+            });
+        });
     
-  
-    'past.webp',
-    'lago.webp',
-    'florez.webp',
-
-  
-   
-  ];
-
-  let currentIndex = 0;
-
-  function changeBackground() {
-    const collageContainer = document.getElementById('collage-container');
-    collageContainer.style.backgroundImage = `url('${backgroundImages[currentIndex]}')`;
-    collageContainer.style.backgroundSize = 'cover';
-
-    currentIndex = (currentIndex + 1) % backgroundImages.length;
-  }
-
-  document.addEventListener('DOMContentLoaded', changeBackground);
-  document.getElementById('change-background-button').addEventListener('click', changeBackground);
-
-  var storyAudio = new Audio('cuento.mp3');
-  var missionAudio = new Audio('elcollage.wav');
-
-  function playSingleAudio(audio) {
-    storyAudio.pause();
-    storyAudio.currentTime = 0;
-    missionAudio.pause();
-    missionAudio.currentTime = 0;
-
-    audio.play();
-  }
-
-  document.getElementById('storyButton').addEventListener('click', function() {
-    if (storyAudio.paused) {
-      playSingleAudio(storyAudio);
-    }
-  });
-
-  document.getElementById('misionButton').addEventListener('click', function() {
-    if (missionAudio.paused) {
-      playSingleAudio(missionAudio);
-    }
-  });
-
-  document.getElementById('play-button').addEventListener('click', function() {
-    const allImages = document.querySelectorAll('#collage-container img');
-    allImages.forEach(img => {
-      const audioSrc = collageImageAudioMap.get(img);
-      if (audioSrc) {
-        playAudio(audioSrc);
-      }
-    });
-  });
-        
